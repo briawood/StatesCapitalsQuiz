bf467a69809c0b54fe38c566be349594b9c06792
briawood <brian@briansmusic.com>
2024-07-22 16:03:42 -0600

    Fix connection string

--- LoginForm.cs
+++ LoginForm.cs
@@ -10,8 +10,6 @@ namespace StatesCapitalsQuiz
             InitializeComponent();
         }
 
-        SqlConnection conn = new(@"Data Source=DESKTOP-5JT7K0O\SQLEXPRESS;Initial Catalog=ProgrammerTest;Integrated Security=True;Encrypt=False;");
-
         private void LoginForm_Load(object sender, EventArgs e)
         {
 
@@ -20,18 +18,27 @@ namespace StatesCapitalsQuiz
         private void BtnLogin_Click(object sender, EventArgs e)
         {
             String username, user_password;
+            //Connection String
+            string cs = @"Data Source=DESKTOP-5JT7K0O\SQLEXPRESS;Initial Catalog=ProgrammerTest;Integrated Security=True;Encrypt=False;";
 
             username = txt_username.Text;
             user_password = txt_password.Text;
 
             try
             {
-                String query = "SELECT * FROM User WHERE UserName = '"+txt_username.Text+"' AND Password = '"+txt_password.Text+"'";
-                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-
-                if (dt.Rows.Count > 0)
+                //Connect to database and check if user exists
+                SqlConnection conn = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM \"User\" WHERE UserName=@username and Password=@password",conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", user_password);
+                conn.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                conn.Close();
+                int count = ds.Tables[0].Rows.Count;
+                //If count is equal to 1, than show frmMain form
+                if (count == 1)
                 {
                     username = txt_username.Text.Trim();
                     user_password = txt_password.Text.Trim();
@@ -53,10 +60,6 @@ namespace StatesCapitalsQuiz
             catch
             {
                 MessageBox.Show("Error");
-            }
-            finally
-            {
-                conn.Close();
             }
         }
         private void BtnClear_Click(object sender, EventArgs e)
